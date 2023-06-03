0f9bc90db002eab301bba8809467ef80c910ad31
xAdiro <adiro.rostek@gmail.com>
2023-06-03 03:35:38 +0200

    Made StateFleeing more clear

--- Assets/Scripts/Units/States/StateFleeing.cs
+++ Assets/Scripts/Units/States/StateFleeing.cs
@@ -14,6 +14,32 @@ public class StateFleeing : IState
 
     public void UpdateState(StateController sc)
     {
+        CheckForEnemies(sc);
+
+        escapeVector = Vector2.zero;
+        if (sc.detectedEnemies.Count > 0)
+        {
+            CalculateEscapeVector(sc);
+        }
+        else
+        {
+            sc.ChangeState(sc.stateWandering);
+        }
+    }
+    
+    public void OnExit(StateController sc)
+    {
+        sc.detectedEnemies.Clear();
+    }
+
+    private IEnumerator fleeingTimer(StateController sc)
+    {
+        yield return new WaitForSeconds(4);
+        sc.ChangeState(sc.stateWandering);
+    }
+
+    private void CheckForEnemies(StateController sc)
+    {
         Collider2D[] colliders = Physics2D.OverlapCircleAll(sc.transform.position, sc.detectionRadius);
 
         foreach (Collider2D collider in colliders)
@@ -23,33 +49,23 @@ public class StateFleeing : IState
                 sc.detectedEnemies.Add(collider.gameObject.transform.position);
             }
         }
+    }
 
-        escapeVector = Vector2.zero;
-        if (sc.detectedEnemies.Count >= 1)
+    private void CalculateEscapeVector(StateController sc)
+    {
+        foreach (Vector2 enemy in sc.detectedEnemies)
         {
-            foreach (Vector2 enemy in sc.detectedEnemies)
-            {
-                Vector2 difference = (enemy - sc.rb.position); // reverse direction
-                escapeVector -= difference / (difference.sqrMagnitude * 2); // subtract instead of add to further reverse direction
-                escapeVector -= (new Vector2(-difference.y, difference.x)) * 0.001f;
-            }
-            
-            escapeVector.Normalize(); // ensure the escape vector is a unit vector
-            sc.rb.velocity = escapeVector *sc.thisUnitController.maxSpeed; // velocity with direction of escape vector and magnitude R
+            Vector2 difference = (enemy - sc.rb.position); // reverse direction
+            escapeVector -= difference / (difference.sqrMagnitude * 2); // subtract instead of add to further reverse direction
+            escapeVector -= (new Vector2(-difference.y, difference.x)) * 0.001f;
         }
-    }
-    
-    public void OnExit(StateController sc)
-    {
-        sc.detectedEnemies.Clear();
-    }
-
-    IEnumerator fleeingTimer(StateController sc)
-    {
-        yield return new WaitForSeconds(4);
-        sc.ChangeState(sc.stateWandering);
+            
+        escapeVector.Normalize(); // ensure the escape vector is a unit vector
+        sc.rb.velocity = escapeVector *sc.thisUnitController.maxSpeed; // velocity with direction of escape vector and magnitude R
     }
 }
+
+
 
 
 
