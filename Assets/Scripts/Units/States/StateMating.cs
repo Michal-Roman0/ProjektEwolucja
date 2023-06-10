59e33be409c1f022d8757ed83831942152b47886
Szymon-Romanczuk <s205859@sggw.edu.pl>
2023-06-10 18:52:07 +0200

    draft of procretion algorithm
    this probalby doesnt work, but shows (i hope) the way it should work

--- Assets/Scripts/Units/States/StateMating.cs
+++ Assets/Scripts/Units/States/StateMating.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class StateMating : IState
 {
+    private GameObject child = null;
+
     public void OnEnter(StateController sc)
     {
         //wejscie w stanWandering
@@ -17,12 +19,63 @@ public class StateMating : IState
     {
         //wyjcie z tego stanu
         // spawnowanie nowej jednostki
+        Instantiate(chlid, sc.transform.position, sc.transform.rotation)
     }
     
     IEnumerator matingTimer(StateController sc)
     {
         yield return new WaitForSeconds(4);
         // TODO tutaj wywołanie algorytmu kopulacji
+        child = Procrate(thisUnitController, selectedTarget)
         sc.ChangeState(sc.stateWandering);
     }
+
+    private GameObject Procrate(firtsParent, secondParent)
+    {
+        Random rnd = new Random();
+        int start = rnd.Next(7);
+        int end = rnd.Next(7);
+
+        if( end < start)
+        {
+            int tmp = end;
+            end = start;
+            start = tmp;
+
+            GameObject tmpParent = firstParent;
+            secondParent = firtsParent;
+            firstParent = tmpParent;
+        }
+
+        float[] newBaseStats = {
+            firstParent.baseStats.agility,
+            firstParent.baseStats.strength,
+            firstParent.baseStats.stealth,
+            firstParent.baseStats.sight,
+            firstParent.baseStats.sense,
+            firstParent.baseStats.size,
+            firstParent.baseStats.morality,
+        };
+        float[] secondBaseStats = {
+            secondParent.baseStats.agility,
+            secondParent.baseStats.strength,
+            secondParent.baseStats.stealth,
+            secondParent.baseStats.sight,
+            secondParent.baseStats.sense,
+            secondParent.baseStats.size,
+            secondParent.baseStats.morality,
+        };
+        for (int i = start; i <= end; i++)
+        {
+            baseStats = secondBaseStats[i];
+        }
+
+        eatsMeat = firstParent.baseStats.eatsMeat;
+        eatsPlants = firstParent.baseStats.eatsPlants;
+
+        
+
+    }
+
 }
+
