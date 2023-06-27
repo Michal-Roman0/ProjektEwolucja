ab54b121d2a285eca127cf92f4cf0f5a90c8615e
MariusAKS <s205863@sggw.edu.pl>
2023-06-27 18:44:53 +0200

    Fixed default values not working

--- Assets/Scripts/UI/MainMenu_Controller.cs
+++ Assets/Scripts/UI/MainMenu_Controller.cs
@@ -72,26 +72,26 @@ public class MainMenu_Controller : MonoBehaviour
         Organisms_Number_Value = Organisms_Panel.transform.Find("Number_Value").GetComponent<TextMeshProUGUI>();
         Organisms_Proportion_Value = Organisms_Panel.transform.Find("Proportion_Value").GetComponent<TextMeshProUGUI>();
 
-        UpdateHerbivoreAgilityValue1(0.1f);
-        UpdateHerbivoreAgilityValue2(0.1f);
-        UpdateHerbivoreSightValue1(0.1f);
-        UpdateHerbivoreSightValue2(0.1f);
-        UpdateHerbivoreSizeValue1(0.1f);
-        UpdateHerbivoreSizeValue2(0.1f);
-        UpdateHerbivoreStrengthValue1(0.1f);
-        UpdateHerbivoreStrengthValue2(0.1f);
+        UpdateHerbivoreAgilityValue1(0.8f);
+        UpdateHerbivoreAgilityValue2(1.2f);
+        UpdateHerbivoreSightValue1(0.8f);
+        UpdateHerbivoreSightValue2(1.2f);
+        UpdateHerbivoreSizeValue1(0.8f);
+        UpdateHerbivoreSizeValue2(1.2f);
+        UpdateHerbivoreStrengthValue1(0.8f);
+        UpdateHerbivoreStrengthValue2(1.2f);
 
-        UpdateCarnivoreAgilityValue1(0.1f);
-        UpdateCarnivoreAgilityValue2(0.1f);
-        UpdateCarnivoreSightValue1(0.1f);
-        UpdateCarnivoreSightValue2(0.1f);
-        UpdateCarnivoreSizeValue1(0.1f);
-        UpdateCarnivoreSizeValue2(0.1f);
-        UpdateCarnivoreStrengthValue1(0.1f);
-        UpdateCarnivoreStrengthValue2(0.1f);
+        UpdateCarnivoreAgilityValue1(0.8f);
+        UpdateCarnivoreAgilityValue2(1.2f);
+        UpdateCarnivoreSightValue1(0.8f);
+        UpdateCarnivoreSightValue2(1.2f);
+        UpdateCarnivoreSizeValue1(0.8f);
+        UpdateCarnivoreSizeValue2(1.2f);
+        UpdateCarnivoreStrengthValue1(0.8f);
+        UpdateCarnivoreStrengthValue2(1.2f);
 
-        UpdateOrganismsNumberValue(10);
-        UpdateOrganismsProportionValue(0);
+        UpdateOrganismsNumberValue(100);
+        UpdateOrganismsProportionValue(0.5f);
     }
 
 
